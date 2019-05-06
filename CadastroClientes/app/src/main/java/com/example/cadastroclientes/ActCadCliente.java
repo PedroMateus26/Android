5b2193a01b739b2c5f650d7a5c4cc2a220d5924d
PedroMateus26 <pedromateusneto@gmail.com>
2019-05-06 12:35:05 -0300

    Menu OK e cancelar criados

--- CadastroClientes/app/src/main/java/com/example/cadastroclientes/ActCadCliente.java
+++ CadastroClientes/app/src/main/java/com/example/cadastroclientes/ActCadCliente.java
@@ -5,8 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActCadCliente extends AppCompatActivity {
     private EditText edtNome;
@@ -24,8 +28,27 @@ public class ActCadCliente extends AppCompatActivity {
         edtEmail = (EditText) findViewById(R.id.edtEmail);
         edtTelefone = (EditText) findViewById(R.id.edtTelefone);
 
-
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_act_cad_cliente, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_ok:
+                Toast.makeText(this, "Botão OK selecionado", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_cancelar:
+                Toast.makeText(this, "Botão Cancelar selecionado", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
